9ced6cec4549b77a832d6a86c872086a3a1518a5
maualkla <mauricio.alcala.4@gmail.com>
2023-04-01 22:58:15 -0600

    wakign

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 ## Sample code
 
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/helloworld')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    
+@app.route('/service', methods=['GET'])
+def service():
+    data = request.get_json()
+    if data is None:
+        return jsonify({})
+    else:
+        return jsonify(data)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
