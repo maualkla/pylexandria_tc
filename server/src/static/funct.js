8169bd377de38b390d5c1ef6494384fbd23e6b24
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-04-25 18:31:51 -0600

    v0_01

--- server/src/static/funct.js
+++ server/src/static/funct.js
@@ -1,1 +1,8 @@
-window.alert('Js working fine')
+// TEST To-be deleted CODE
+
+// window.alert('Js working fine')
+
+    // listener for the login button to confirm JS funcitons
+    document.getElementById('B_Login').addEventListener('click', function (){
+        console.log("Js is working fine");
+    });
