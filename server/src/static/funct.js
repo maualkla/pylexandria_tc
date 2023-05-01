6f4e5ca24766ca742c42e50743383f6112f8787d
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-04-30 21:23:22 -0600

    endoftheday

--- server/src/static/funct.js
+++ server/src/static/funct.js
@@ -6,3 +6,8 @@
     document.getElementById('B_Login').addEventListener('click', function (){
         console.log("Js is working fine");
     });
+
+    // listener for the login button to confirm JS funcitons
+    document.getElementById('M_Login').addEventListener('click', function (){
+        console.log("Js is working fine");
+    });
